918962308a369d06fe5f2a70c022d0b5234bce80
Hermann Elton <60223374+Her0elt@users.noreply.github.com>
2021-11-03 12:29:02 +0100

    feat(setup): init model, agent, and env made (#1)

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
-from nes_py.wrappers import JoypadSpace
-import gym_super_mario_bros
-from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
+from agent import DQNAgent
+from environment import MarioEnvironment
+    
 
-done = True
-for step in range(5000):
-    if done:
-        state = env.reset()
-    state, reward, done, info = env.step(env.action_space.sample())
-    env.render()
+def train(env, agent, episodes):
+    frame_count = 0
+    score = 0
+    scores = []
+    num_episode_steps = env.spec.max_episode_steps
+    max_reward = 0
+    for _ in range(episodes):
+        done = False
+        state = agent.preprocess_state(env.reset())
+        for _ in range(num_episode_steps):
+            action = agent.action(state)
+            next_state, reward, done, _ = env.step(action) 
+            env.render(mode="human")
+            next_state =  agent.preprocess_state(next_state)
+            agent.remember(state, action, reward, next_state, done)
+            score += reward
+            state = next_state
+            if done:
+                break
+            if frame_count % 4 == 0:
+                agent.replay()
+            frame_count+=1
+        if score > max_reward:
+            agent.model.save_model('dqn_mario_agent_v0.h5')
 
-env.close()
+        scores.append(reward)
+    print('Finished training!')
+    env.close()
+
+env = MarioEnvironment()
+state_space = env.observation_space.shape
+action_space = env.action_space.n
+agent = DQNAgent(env, state_space, action_space)
+train(env, agent,  episodes=50000)
---
+++ model.py
@@ -0,0 +1,34 @@
+
+from typing import Any
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Conv2D, Flatten
+from tensorflow.keras.optimizers import  RMSprop
+from tensorflow.keras.models import load_model
+
+import numpy as np
+
+
+class DQN:
+    def __init__(self, input_shape: tuple, action_shape: int):
+        self.model = Sequential()
+        self.model.add(Conv2D(32, (8, 8), strides=(4, 4), activation='relu', input_shape=input_shape))
+        self.model.add(Conv2D(64, (4, 4), strides=(2, 2), activation='relu'))
+        self.model.add(Conv2D(64, (3, 3), strides=(1, 1), activation='relu'))
+        self.model.add(Flatten())
+        self.model.add(Dense(512, activation='relu', kernel_initializer='random_uniform'))
+        self.model.add(Dense(action_shape, activation="softmax"))   
+        self.model.compile(loss="mse", optimizer=RMSprop(
+            learning_rate=0.0001), metrics=["accuracy"])
+    
+    def fit(self, x: np.ndarray, y: np.ndarray, batch_size: int, verbose: 0 | 1):
+        self.model.fit(x, y, batch_size=batch_size, verbose=verbose)
+    
+    def predict(self, x: np.ndarray) -> Any:
+        return self.model.predict(x)
+
+    def save_model(self, file_name: str):
+        self.model.save(file_name)
+
+    def load_model(self, file_name: str):
+        self.model = load_model(file_name)
+        
