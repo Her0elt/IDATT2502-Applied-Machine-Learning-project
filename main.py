77b3a71ea01e2dcd2ad07348703402f7ee6c522f
her0elt <hermann.owren.elton@hotmail.com>
2021-11-05 12:30:38 +0100

    run function

--- main.py
+++ main.py
@@ -34,8 +34,21 @@ def train(env, agent, episodes):
     print('Finished training!')
     env.close()
 
+def run(env, agent, model_name):
+    agent.model.load_model(model_name)
+    num_episode_steps = env.spec.max_episode_steps
+    state = agent.preprocess_state(env.reset())
+    for _ in range(num_episode_steps):
+        action = agent.action(state)
+        next_state, reward, done, _ = env.step(action)
+        env.render(mode="human")
+        next_state =  agent.preprocess_state(next_state)
+        state = next_state
+    env.close()
+    
 env = create_mario_env()
 state_space = env.observation_space.shape
 action_space = env.action_space.n
 agent = DQNAgent(env, state_space, action_space)
-train(env, agent,  episodes=500)
+# train(env, agent,  episodes=500)
+run(env, agent, "dqn_mario_agent_v0.h5")
