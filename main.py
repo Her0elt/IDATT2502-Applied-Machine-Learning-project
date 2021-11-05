35ec6833d815f60b43650883ec9215fd0fdad947
her0elt <hermann.owren.elton@hotmail.com>
2021-11-05 12:48:25 +0100

    feat(formatter): added formatting and linting job

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from agent import DQNAgent
 from environment import MarioEnvironment
-    
+
 
 def train(env, agent, episodes):
     frame_count = 0
@@ -13,9 +13,9 @@ def train(env, agent, episodes):
         state = agent.preprocess_state(env.reset())
         for _ in range(num_episode_steps):
             action = agent.action(state)
-            next_state, reward, done, _ = env.step(action) 
+            next_state, reward, done, _ = env.step(action)
             env.render(mode="human")
-            next_state =  agent.preprocess_state(next_state)
+            next_state = agent.preprocess_state(next_state)
             agent.remember(state, action, reward, next_state, done)
             score += reward
             state = next_state
@@ -23,16 +23,17 @@ def train(env, agent, episodes):
                 break
             if frame_count % 4 == 0:
                 agent.replay()
-            frame_count+=1
+            frame_count += 1
         if score > max_reward:
-            agent.model.save_model('dqn_mario_agent_v0.h5')
+            agent.model.save_model("dqn_mario_agent_v0.h5")
 
         scores.append(reward)
-    print('Finished training!')
+    print("Finished training!")
     env.close()
+
 
 env = MarioEnvironment()
 state_space = env.observation_space.shape
 action_space = env.action_space.n
 agent = DQNAgent(env, state_space, action_space)
-train(env, agent,  episodes=50000)
+train(env, agent, episodes=50000)
--- model.py
+++ model.py
@@ -1,28 +1,33 @@
-
 from typing import Any
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, Flatten
-from tensorflow.keras.optimizers import  RMSprop
-from tensorflow.keras.models import load_model
 
 import numpy as np
+from tensorflow.keras.layers import Conv2D, Dense, Flatten
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.optimizers import RMSprop
 
 
 class DQN:
     def __init__(self, input_shape: tuple, action_shape: int):
         self.model = Sequential()
-        self.model.add(Conv2D(32, (8, 8), strides=(4, 4), activation='relu', input_shape=input_shape))
-        self.model.add(Conv2D(64, (4, 4), strides=(2, 2), activation='relu'))
-        self.model.add(Conv2D(64, (3, 3), strides=(1, 1), activation='relu'))
+        self.model.add(
+            Conv2D(
+                32, (8, 8), strides=(4, 4), activation="relu", input_shape=input_shape
+            )
+        )
+        self.model.add(Conv2D(64, (4, 4), strides=(2, 2), activation="relu"))
+        self.model.add(Conv2D(64, (3, 3), strides=(1, 1), activation="relu"))
         self.model.add(Flatten())
-        self.model.add(Dense(512, activation='relu', kernel_initializer='random_uniform'))
-        self.model.add(Dense(action_shape, activation="softmax"))   
-        self.model.compile(loss="mse", optimizer=RMSprop(
-            learning_rate=0.0001), metrics=["accuracy"])
-    
+        self.model.add(
+            Dense(512, activation="relu", kernel_initializer="random_uniform")
+        )
+        self.model.add(Dense(action_shape, activation="softmax"))
+        self.model.compile(
+            loss="mse", optimizer=RMSprop(learning_rate=0.0001), metrics=["accuracy"]
+        )
+
     def fit(self, x: np.ndarray, y: np.ndarray, batch_size: int, verbose: 0 | 1):
         self.model.fit(x, y, batch_size=batch_size, verbose=verbose)
-    
+
     def predict(self, x: np.ndarray) -> Any:
         return self.model.predict(x)
 
@@ -31,4 +36,3 @@ class DQN:
 
     def load_model(self, file_name: str):
         self.model = load_model(file_name)
-        
