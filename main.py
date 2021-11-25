fddebcdcd590a802391327110facba8d8944fd41
Hermann Elton <60223374+Her0elt@users.noreply.github.com>
2021-11-25 18:19:19 +0100

    format

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from src.constants import GAMMA, MIN_EPSILON
-from src.trainers.dqn_trainer import run as DQN_run
+# from src.constants import GAMMA, MIN_EPSILON
+# from src.trainers.dqn_trainer import run as DQN_run
 
 from src.trainers.ppo_trainer import run as PPO_Run
-from src.constants import GAMMA, MIN_EPSILON, PPO_GAMMA, PPO_LAMBDA, WORLD
+from src.constants import GAMMA, MIN_EPSILON, PPO_GAMMA, PPO_LAMBDA
 
 # DQN_run(
 #     pretrained=False,
